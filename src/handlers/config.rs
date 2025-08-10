72f52ff8fa44b194da8446ae8d7ff55f742bef20
Xithrius <xithrius@fastmail.com>
2025-08-10 15:03:18 -0700

    Updated dependencies, added CI caching, fixed devcontainer

--- src/handlers/config.rs
+++ src/handlers/config.rs
@@ -1,10 +1,10 @@
 use std::{
-    fs::{create_dir_all, read_to_string, File},
+    fs::{File, create_dir_all, read_to_string},
     io::Write,
     path::Path,
 };
 
-use color_eyre::eyre::{bail, Error, Result};
+use color_eyre::eyre::{Error, Result, bail};
 use serde::{Deserialize, Serialize};
 
 use crate::utils::pathing::config_path;
@@ -25,7 +25,9 @@ impl CompleteConfig {
             let mut file = File::create(path_str.clone()).unwrap();
             file.write_all(default_toml_string.as_bytes()).unwrap();
 
-            bail!("Configuration was generated at {path_str}, please fill it out with necessary information.")
+            bail!(
+                "Configuration was generated at {path_str}, please fill it out with necessary information."
+            )
         } else if let Ok(config_contents) = read_to_string(p) {
             let config: Self = toml::from_str(config_contents.as_str()).unwrap();
 
@@ -35,7 +37,10 @@ impl CompleteConfig {
         } else {
             bail!(
                 "Configuration could not be read correctly. See the following link for the example config: {}",
-                format!("{}/blob/main/default-config.toml", env!("CARGO_PKG_REPOSITORY"))
+                format!(
+                    "{}/blob/main/default-config.toml",
+                    env!("CARGO_PKG_REPOSITORY")
+                )
             )
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,9 @@ mod utils;
 use crate::handlers::config::CompleteConfig;
 
 fn main() -> Result<(), Error> {
-    color_eyre::install().unwrap();
+    color_eyre::install()?;
 
-    let mut _config = CompleteConfig::new()
-        .wrap_err("Unable to read configuration file.")
-        .unwrap();
+    let mut _config = CompleteConfig::new().context("Unable to read configuration file.")?;
 
     std::process::exit(0)
 }
